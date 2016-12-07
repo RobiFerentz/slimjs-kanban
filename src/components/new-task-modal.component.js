3efaa82f2f6b8f9e978234a512578d074138b860
omeb <iamomerburshtein@gmail.com>
2016-12-07 11:24:00 +0200

    added two components

---
+++ src/components/new-task-modal.component.js
@@ -0,0 +1,27 @@
++(function() {
+    Slim('new-task-modal', class extends SlimBaseElement {
+
+        get updateOnAttributes() {
+            return ['onclick', 'text']
+        }
+
+        get template() {
+            return `<input type="button" />`
+        }
+
+        render() {
+            this.onclick = undefined
+            this.find('input').onclick = (event) => {
+                let methodName = this.getAttribute('onclick')
+                this.parentBind[this.getAttribute('onclick')](event)
+            }
+            this.update()
+        }
+
+        update() {
+            this.find('input').value = this.getAttribute('text')
+        }
+
+
+    })
+}())

7f127c7e4f1cec8fa0928cb3bb4dfc61af3b125f
Robert Ferentz <RobiFerentz@gmail.com>
2016-12-07 12:26:04 +0200

    work on repeated elements

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@
 
 (function () {
     Slim('app-root', class extends SlimBaseElement {
-        set model (value) {
-            this._model = value;
-            this.model.load();
-            this.model.addEventListener('change', update.bind(this))
+
+        afterRender() {
+            console.log(this.model)
         }
-        get model () {
-            return this._model;
+
+        // set model(value){
+        //     this._model = value;
+        // }
+        //
+        // get model() {
+        //     return this._model;
+        // }
+
+        get columns() {
+            return [{ name: 'col 1'}, { name: 'col 2'}, { name: 'col 3'}]
+        }
+
+        get template() {
+            return `<s-repeat source="columns"><task-column></task-column></s-repeat>`;
         }
 
     })

1f3dcbe47117f19128adab62bce248f5eef88f4b
Dror Ben-Gai <dbengai@gmail.com>
2016-12-07 16:53:13 +0200

    content type json

--- dbserver.js
+++ dbserver.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json())
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    res.header('Content-Type', 'application/json')
     next();
 })
 

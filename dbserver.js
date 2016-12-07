78de00a310acd9b1984022d7f2b973dbfaa1d3c4
Dror Ben-Gai <dbengai@gmail.com>
2016-12-07 12:02:29 +0200

    dbserver listening on port 3003

--- dbserver.js
+++ dbserver.js
@@ -52,6 +52,6 @@ app.get('/column/get', function(req, res) {
         })
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(3003, function () {
+    console.log('DB server listening on port 3003!')
 })
